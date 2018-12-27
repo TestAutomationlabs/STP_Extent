baac170d14cd1b054374ae5ddf87850881d916e0
Sagar Lankegowda <sagar.lankegowda@external.merckgroup.com>
2018-12-27 17:17:17 +0530

    Changes done for MandatoryFieldValidation

--- src/test/java/com/qa/testcases/PostButtonTest.java
+++ src/test/java/com/qa/testcases/PostButtonTest.java
@@ -1,14 +1,20 @@
 package com.qa.testcases;
 
+import java.util.Set;
+
+
+
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.qa.Base.TestBase;
 import com.qa.pages.HomePage;
+import com.qa.pages.PostPage;
 
 public class PostButtonTest extends TestBase{
 	HomePage homepage;
+	PostPage post;
 	
 	
 	public PostButtonTest()
@@ -20,14 +26,24 @@ public class PostButtonTest extends TestBase{
 	public void setUp() {
 		initialization();
 		homepage=new HomePage();
+		post = new PostPage();
 	}
 	
 	
-  @Test
+  @Test(priority=1)
 	public void AddPostTest()
 	{
 			homepage.ClickOnPostButton();
 			System.out.println("Navigated to the AddPost");
-		}
+	}
+  @Test(priority=2)
+  public void verifyPostPageTest() throws Exception
+  {
+	  post.verifycategoryBox();
+	  post.verifyaddpostdetails();
+	  post.verifyPost();
+//  }
 }
+}
+
   
