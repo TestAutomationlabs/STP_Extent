90de7b8c7ee026233bf8727a199dfe6207754a7d
Sagar Lankegowda <sagar.lankegowda@external.merckgroup.com>
2018-12-27 19:50:45 +0530

    Day 27/12

--- src/test/java/com/qa/testcases/STPDetailsVAlidationTest.java
+++ src/test/java/com/qa/testcases/STPDetailsVAlidationTest.java
@@ -6,8 +6,8 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import com.qa.Base.TestBase;
-import com.qa.Pages.HomePage;
-import com.qa.Pages.STPDetailsValidationPage;
+import com.qa.pages.HomePage;
+import com.qa.pages.STPDetailsValidationPage;
 
 
 
@@ -23,12 +23,12 @@ STPDetailsValidationPage stpdetails;
 	@BeforeClass
 	public void setUp() {
 		initialization();
-		landingpage=new LandingPage();
+		homepage=new HomePage();
 	}
 	
 	@Test(priority=1)
 	public void CreateSTPValidateTest() {
-		landingpage.ClickOnTPLink();
+		homepage.ClickOnSTPLink();
 		System.out.println("User tap on STPButton");
 	}
 	
