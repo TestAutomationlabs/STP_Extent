2393673425da270f5b51db641beea36eae4ab63d
Uday Kumar <uday.kumar@external.merckgroup.com>
2019-01-08 14:37:47 +0530

    Extent Report classes

--- src/test/java/com/qa/testcases/CreateSTPPagetest.java
+++ src/test/java/com/qa/testcases/CreateSTPPagetest.java
@@ -1,14 +1,20 @@
 package com.qa.testcases;
 
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.qa.Base.TestBase;
+import com.qa.Util.ExtentContent;
 import com.qa.pages.CreateSTPPage;
 import com.qa.pages.HomePage;
+import com.qa.pages.STPDetails;
 import com.qa.pages.STPDetailsValidationPage;
 import com.qa.pages.ToastMessages;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class CreateSTPPagetest extends TestBase {
 
@@ -16,6 +22,9 @@ public class CreateSTPPagetest extends TestBase {
 	HomePage home;
 	ToastMessages toast;
 	STPDetailsValidationPage validation;
+	STPDetails details;
+	ExtentTest test;
+	ExtentReports report;
 	
 	public CreateSTPPagetest()
 	{
@@ -28,13 +37,16 @@ public class CreateSTPPagetest extends TestBase {
 		create = new CreateSTPPage();
 		home = new HomePage();
 		toast = new ToastMessages();
-		validation = new STPDetailsValidationPage();
-		
+		//	validation = new STPDetailsValidationPage();
+		details = new STPDetails();
+		String classnm = getClass().getSimpleName();
+		report = ExtentContent.reporter(classnm);
 	}
 
 	@Test(priority = 1)
 	public void HelpTextValidation() throws Exception {
-
+		//Reporter		
+		test = report.startTest("Create STP Help Text Validation");
 		home.ClickOnSTPLink();
 		Thread.sleep(1000);
 		create.HelpToggleValidation();
@@ -45,6 +57,7 @@ public class CreateSTPPagetest extends TestBase {
 
 	public void MandatoryFieldValidation() throws Exception
 	{
+		//test = report.startTest("Create STP Mandatory field validation");
 		for (int i=1; i<6 ; i++)
 		{
 		create.EnterMandatoryFields(i);
@@ -52,7 +65,8 @@ public class CreateSTPPagetest extends TestBase {
 		toast.DescriptionToast(i);
 		toast.CommunityOrganiserToast(i);
 		toast.successfulToast(i);
-		//validation.detailsValidation(5);
+		details.detailsValidation(5);
+		
 		
 		}
 	}
@@ -60,19 +74,34 @@ public class CreateSTPPagetest extends TestBase {
 	@Test(priority = 3)
 	public void AllFieldValidation() throws Exception
 	{
+		//test = report.startTest("Create STP All Field Validation");
 		home.ClickOnSTPLink();
 		Thread.sleep(1000);
 		create.EnterALLFields();
 		Thread.sleep(3000);
 		toast.successfulToast(6);
-		validation.detailsValidation(6);
+		//validation.detailsValidation(6);
 		
 		
 	}
 
 @AfterClass
-public void CloseBrowser()
+public void CloseBrowser(ITestResult result)
 {
+	if (result.getStatus() == ITestResult.FAILURE)
+	{
+		test.log(LogStatus.FAIL, "Test Failed at "+ result.getName());	
+		test.log(LogStatus.FAIL, "Error accured was:"+ result.getThrowable());
+	}
+	else if (result.getStatus() == ITestResult.SKIP)
+	{
+		test.log(LogStatus.SKIP, "Test skipped at "+ result.getName());
+		test.log(LogStatus.SKIP, "Error accured was:"+ result.getThrowable());
+	}
+	report.endTest(test);
+	report.flush();
+	report.close();
+	
 	driver.close();
 }
 }
